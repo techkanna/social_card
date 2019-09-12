155b17efa897247cd03fb5a442e45a3b0455be93
Senthilkannan <senthilkannanarmy@gmail.com>
2019-09-12 04:44:54 -0700

    Updates

---
+++ precache-manifest.eb857be60c42656d9aba1349160c50b4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b864acd1c58b9cc0c7e52c199156e9cf",
+    "url": "/social_card/index.html"
+  },
+  {
+    "revision": "633ecd4ef108f601e2cc",
+    "url": "/social_card/static/css/main.296ef450.chunk.css"
+  },
+  {
+    "revision": "6f50c8d8d4e69208650a",
+    "url": "/social_card/static/js/2.ef1681a4.chunk.js"
+  },
+  {
+    "revision": "633ecd4ef108f601e2cc",
+    "url": "/social_card/static/js/main.3b61d85a.chunk.js"
+  },
+  {
+    "revision": "1b8bceba20b47cc88fc4",
+    "url": "/social_card/static/js/runtime~main.494993cd.js"
+  },
+  {
+    "revision": "704fc8761429b9490f9394e3d9b14690",
+    "url": "/social_card/static/media/me.704fc876.jpg"
+  }
+]);
